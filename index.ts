354430fc2c56273fc21d8e0c9c765717385b1c33
Niklas <nlinnanen@gmail.com>
2023-01-25 19:32:34 +0200

    Add basic functionality

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
 import * as dotenv from 'dotenv';
+import { isMessage } from "./validators";
 
 dotenv.config();
 
@@ -9,10 +10,26 @@ if (!process.env.BOT_TOKEN) {
 }
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.start((ctx) => ctx.reply('Welcome'));
-bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+
+let chatsWaiting = new Set()
+
+
+bot.command('entry', async (ctx, next) => {
+    chatsWaiting.add(ctx.chat.id)
+    await ctx.sendMessage('Hey there! What distance did you travel')
+})
+
+bot.on('message', (ctx: any, next) => {
+    const number = parseInt(ctx.update.message?.text)
+    if(chatsWaiting.has(ctx.chat.id) && !isNaN(number)){
+        console.log(number)
+        chatsWaiting.delete(ctx.chat.id)
+        ctx.reply(`Number: ${number}`)
+    } else if (chatsWaiting.has(ctx.chat.id)) {
+        ctx.reply('Not a number')
+    }
+});
+
 bot.launch();
 
 // Enable graceful stop
