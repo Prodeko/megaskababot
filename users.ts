37fdf89c7e31343d1dc8f09811e8cb1294d61c86
Niklas <nlinnanen@gmail.com>
2023-01-26 20:17:12 +0200

    Add db integration

--- users.ts
+++ users.ts
@@ -1,9 +1,14 @@
+import { prisma } from "./config"
 import { writeToDb } from "./db"
 import { User } from "./types"
+import { isCompleteUser } from "./validators"
 
 const users = new Map<number, Partial<User>>()
 
-const isUser = (userId: number) => users.has(userId)
+const isUser = async (userId: number) => {
+    const user = await prisma.user.findUnique({where: {telegramUserId: userId}})
+    return !!user
+}
 
 const updateUsersStash = (userId: number, update: Partial<User>) => {
     users.set(userId, {
@@ -12,10 +17,20 @@ const updateUsersStash = (userId: number, update: Partial<User>) => {
     })
 } 
 
-const usersToDb = () => writeToDb('users_db.json', users)
+const userToDb = async (userId: number) => {
+    const user = users.get(userId)
+    if (!user || !isCompleteUser(user)){
+        console.log(user)
+        throw new Error("User not complete")
+    } 
+
+    await prisma.user.create({
+        data: user
+    })
+}
 
 export {
     isUser,
     updateUsersStash,
-    usersToDb
+    userToDb
 }
--- validators.ts
+++ validators.ts
@@ -1,5 +1,5 @@
 import { GUILDS, SPORTS } from "./constants";
-import { Guild, Sport } from "./types";
+import { Entry, EntryWithoutId, Guild, Sport, User, UserWithoutTime } from "./types";
 
 const isSport = (sport: any): sport is Sport => {
     return typeof sport === 'string' && SPORTS.some(t => sport === t)
@@ -9,7 +9,25 @@ const isGuild = (guild: any): guild is Guild => {
     return typeof guild === 'string' && GUILDS.some(g => guild === g)
 }
 
+const isCompleteUser = (user: Partial<User>): user is UserWithoutTime => {
+    return typeof user?.telegramUserId === "number" && 
+    typeof user?.telegramUsername === "string" &&
+    typeof user?.firstName === "string" &&
+    typeof user?.lastName === "string" &&
+    typeof user?.freshmanYear === "number" &&
+    typeof user?.guild === "string"
+}
+
+const isCompleteEntry = (entry: Partial<Entry>): entry is EntryWithoutId => {
+    return typeof entry?.distance === "number" &&
+    typeof entry?.fileId === "string" &&
+    typeof entry?.userId === "number" &&
+    isSport(entry?.sport)
+}
+
 export {
     isSport,
-    isGuild
+    isGuild,
+    isCompleteUser,
+    isCompleteEntry
 }
