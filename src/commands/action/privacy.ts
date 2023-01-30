01ce71ef6dc7a1441d994f1f7a5ddebcd46fbefa
Niklas <nlinnanen@gmail.com>
2023-01-30 09:33:23 +0200

    Update stash on privacy accepted

--- src/commands/action/privacy.ts
+++ src/commands/action/privacy.ts
@@ -1,11 +1,19 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { START_REGISTRATION_MESSAGE, PRIVACY_REJECTED_MESSAGE } from "../../common/constants";
 import { ActionContext } from "../../common/types";
 import { conversationPhase } from "../../common/variables";
 import { yearKeyboard } from "../../keyboards";
+import { updateUsersStash } from "../../users";
 
 export async function onPrivacyAccepted(ctx: ActionContext) {
   conversationPhase.set(ctx.chat!.id, 'year')
   await ctx.editMessageReplyMarkup(undefined) // Clear inline keyboard
+  updateUsersStash(ctx!.from!.id, {
+    firstName: ctx!.from!.first_name,
+    lastName: ctx!.from!.last_name,
+    telegramUsername: ctx!.from!.username,
+    telegramUserId: ctx!.from!.id,
+  })
   await ctx.reply(START_REGISTRATION_MESSAGE)
   ctx.reply('What is your freshman year?', yearKeyboard)
 }
