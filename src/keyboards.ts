e0673fc98cb6a9e3478a70d54a9f1352236fa240
Niklas <nlinnanen@gmail.com>
2023-01-30 14:43:41 +0200

    Add emojis to keyboards

--- src/keyboards.ts
+++ src/keyboards.ts
@@ -13,11 +13,11 @@ export const guildKeyboard = Markup.keyboard(GUILDS).oneTime()
 export const yearKeyboard = Markup.keyboard(YEARS).oneTime()
 export const transpKeyboard = Markup.keyboard(SPORTS).oneTime().resize()
 export const confirmationKeyboard = Markup.inlineKeyboard([
-  Markup.button.callback('Remove', 'remove'),
-  Markup.button.callback('Cancel', 'cancel'),
+  Markup.button.callback('Remove 🗑️', 'remove'),
+  Markup.button.callback('Cancel 🚫', 'cancel'),
 ])
 
 export const validationKeyboard = Markup.inlineKeyboard([
-  [Markup.button.callback('Valid', 'valid'), Markup.button.callback('Invalid', 'invalid')],
-  [Markup.button.callback('Stop validation', 'stopvalidation')],
+  [Markup.button.callback('Valid ✅', 'valid'), Markup.button.callback('Invalid ❌', 'invalid')],
+  [Markup.button.callback('Stop validation 🛑', 'stopvalidation')],
 ])
