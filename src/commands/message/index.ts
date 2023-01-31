a22e7747f352791f769c4f80f0322d9dc020ad70
Niklas <nlinnanen@gmail.com>
2023-01-31 18:19:36 +0200

    Force distancer under 1000

--- src/commands/message/index.ts
+++ src/commands/message/index.ts
@@ -54,13 +54,15 @@ const message = async (ctx: any, next: () => Promise<void>) => {
       }
 
       const distance = parseFloat(text.replace(",", "."))
-      if (!isNaN(distance) && distance > 0) {
+      if (!isNaN(distance) && distance > 0 && distance < 1000) {
         updateEntryStash(chatId, {
           userId,
           distance,
         })
         await ctx.reply('Please give proof as a picture 📷')
         conversationPhase.set(chatId, 'proof')
+      } else if(distance >= 1000) {
+        await ctx.reply('Are you sure that right 👀? If it is, contact the admins')
       } else {
         await ctx.reply('Please give a positive number 👀')
       }
