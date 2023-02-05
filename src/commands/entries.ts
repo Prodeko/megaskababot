947eb7bc7c1f6f1c7a042958544f60668a7fff49
Niklas <nlinnanen@gmail.com>
2023-02-05 18:16:38 +0200

    List total distance by sport in entries listing

--- src/commands/entries.ts
+++ src/commands/entries.ts
@@ -1,8 +1,10 @@
-import { COEFFICIENTS } from "../common/constants"
-import { ActionContext, CommandContext } from "../common/types"
-import { formatEntry } from "../common/utils"
-import { getEntries } from "../entries"
-import { commandsKeyboard } from "../keyboards"
+import _ from 'lodash'
+
+import { COEFFICIENTS } from '../common/constants'
+import { ActionContext, CommandContext } from '../common/types'
+import { formatEntry } from '../common/utils'
+import { getEntries } from '../entries'
+import { commandsKeyboard } from '../keyboards'
 
 const entries = async (ctx: CommandContext | ActionContext) => {
   const entries = await getEntries(ctx!.from!.id)
@@ -10,17 +12,22 @@ const entries = async (ctx: CommandContext | ActionContext) => {
     await ctx.editMessageReplyMarkup(undefined)
   } catch {}
   if (entries.length > 0) {
-    const points = entries
-      .map(e => e.distance * COEFFICIENTS[e.sport])
-      .reduce((p, e) => p + e, 0)
+    const points = entries.map(e => e.distance * COEFFICIENTS[e.sport]).reduce((p, e) => p + e, 0)
 
     const distance = entries.reduce((p, e) => p + e.distance, 0)
 
-    await ctx.reply(
+    const distanceBySport = Object.entries((_.groupBy(entries, e => e.sport)))
+      .map<[string, number]>(([key, value]) => [key, value.reduce((p, e) => p + e.distance, 0)])
+
+    await ctx.replyWithHTML(
       entries
         .map(formatEntry)
-        .concat([`Total distance: ${distance.toFixed(2)} km\nTotal points: ${points.toFixed(2)} points`])
-        .join('\n\n'), commandsKeyboard
+        .concat(["<strong>Totals</strong>"].concat(distanceBySport.map(([sport, dist]) => `${sport}: ${dist} km`)).join("\n"))
+        .concat([
+          `Total distance: ${distance.toFixed(2)} km\nTotal points: ${points.toFixed(2)} points`,
+        ])
+        .join('\n\n'),
+      commandsKeyboard
     )
   } else {
     await ctx.reply('No entries yet!', commandsKeyboard)
