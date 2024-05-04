ca6fa61a66d6bf5c17210ad3447969e303cc0aca
Niklas Linnanen <nlinnanen@gmail.com>
2024-05-04 15:01:06 +0000

    Move https server to seperate folder

--- src/launchBotDependingOnNodeEnv.ts
+++ src/launchBotDependingOnNodeEnv.ts
@@ -4,11 +4,13 @@ import type { Context, Telegraf } from "telegraf";
 import type { Update } from "telegraf/typings/core/types/typegram";
 
 import { saveEntriesAsCSV } from "./entries";
+import app, { launchServer } from "./server";
 
 /**
  * Launch bot in long polling (development) mode
  */
 async function launchLongPollBot(bot: Telegraf<Context<Update>>) {
+	launchServer();
 	await bot.launch();
 }
 
@@ -16,46 +18,13 @@ async function launchLongPollBot(bot: Telegraf<Context<Update>>) {
  * Launch bot in webhook (production) mode
  */
 async function launchWebhookBot(bot: Telegraf<Context<Update>>) {
-	const port = Number.parseInt(process.env.PORT!);
-
-	// Necessary because of Azure App Service health check on startup
-	const app = express();
-
-	app.get("/", (_req, res) => {
-		res.status(200).send("Kovaa tulee");
-	});
-
-	app.get("/health", (_req, res) => {
-		res.status(200).send("OK");
-	});
-
-	app.get("/entries", async (req, res) => {
-		if (req.query.pass !== process.env.ADMIN_PASSWORD) {
-			console.log("Wrong password");
-			return res.status(401).send("Wrong password!");
-		}
-		console.log("here");
-		await saveEntriesAsCSV();
-		res.attachment("./entries.csv");
-		res.header("Content-Type", "text/csv");
-		res.status(200).send(fs.readFileSync("./entries.csv"));
-	});
-
-	app.get("/statistics", async (req, res) => {
-		if (req.query.pass !== process.env.ADMIN_PASSWORD) {
-			console.log("Wrong password");
-			return res.status(401).send("Wrong password!");
-		}
-	});
-
 	// Workaround to avoid issue with TSconfig
 	const createWebhookListener = async () => {
 		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 		app.use(await bot.createWebhook({ domain: process.env.DOMAIN! }));
 	};
 	await createWebhookListener();
-
-	app.listen(port, () => console.log("Running on port ", port));
+	launchServer();
 }
 
 /**
---
+++ src/server/index.ts
@@ -0,0 +1,41 @@
+import express from "express";
+import { saveEntriesAsCSV } from "../entries";
+
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
+const port = Number.parseInt(process.env.PORT!);
+
+const app = express();
+
+// Necessary because of Azure App Service health check on startup
+app.get("/", (_req, res) => {
+	res.status(200).send("Kovaa tulee");
+});
+
+app.get("/health", (_req, res) => {
+	res.status(200).send("OK");
+});
+
+app.get("/entries", async (req, res) => {
+	if (req.query.pass !== process.env.ADMIN_PASSWORD) {
+		console.log("Wrong password");
+		return res.status(401).send("Wrong password!");
+	}
+	console.log("here");
+	await saveEntriesAsCSV();
+	res.attachment("./entries.csv");
+	res.header("Content-Type", "text/csv");
+	res.status(200).send(fs.readFileSync("./entries.csv"));
+});
+
+app.get("/statistics", async (req, res) => {
+	if (req.query.pass !== process.env.ADMIN_PASSWORD) {
+		console.log("Wrong password");
+		return res.status(401).send("Wrong password!");
+	}
+});
+
+export const launchServer = async () => {
+  app.listen(port, () => console.log("Running on port ", port));
+}
+
+export default app;
