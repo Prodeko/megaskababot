7eef4522dc3eb173b6c835e734eccd152a40276f
mUusitalo <73437699+mUusitalo@users.noreply.github.com>
2025-01-31 19:55:49 +0000

    Set bot metadata on startup

--- src/index.ts
+++ src/index.ts
@@ -47,13 +47,22 @@ bot.use(session({
 
 bot.use(conversations());
 
-await bot.api.setMyCommands([
-  { command: "start", description: "Start the bot" },
-  { command: "help", description: "Show the help message" },
-  { command: "rules", description: "Show the rules of the competition" },
-  { command: "entry", description: "Add a new entry" },
-  { command: "entries", description: "Show your previous entries" },
-  { command: "removelatest", description: "Remove your latest entry" },
+await Promise.all([
+  bot.api.setMyCommands([
+    { command: "start", description: "Start the bot" },
+    { command: "help", description: "Show the help message" },
+    { command: "rules", description: "Show the rules of the competition" },
+    { command: "entry", description: "Add a new entry" },
+    { command: "entries", description: "Show your previous entries" },
+    { command: "removelatest", description: "Remove your latest entry" },
+  ]),
+  bot.api.setMyName("Megaskababot"),
+  bot.api.setMyDescription(
+    "Megaskababot allows you to keep track of your entries in Megaskaba.",
+  ),
+  bot.api.setMyShortDescription(
+    "Megaskababot allows you to keep track of your entries in Megaskaba.",
+  ),
 ]);
 
 const privateBot = bot.chatType("private");
