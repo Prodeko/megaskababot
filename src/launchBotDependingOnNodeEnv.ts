85e8b55eb02e8dee6b6c0bc3f6ec8dc659004473
mUusitalo <73437699+mUusitalo@users.noreply.github.com>
2025-01-31 23:38:08 +0000

    Launch express server even when in long polling mode

--- src/launchBotDependingOnNodeEnv.ts
+++ src/launchBotDependingOnNodeEnv.ts
@@ -6,8 +6,8 @@ import process from "node:process";
  * Launch bot in long polling (development) mode
  */
 async function launchLongPollBot<T extends Context>(bot: Bot<T, Api<RawApi>>) {
-  await bot.start();
   launchServer();
+  bot.start().catch(e => console.error(e));
 }
 
 /**
@@ -22,14 +22,14 @@ function launchWebhookBot<T extends Context>(bot: Bot<T, Api<RawApi>>) {
  * Launches the bot in webhook mode if NODE_ENV is "production", or long polling (development) mode otherwise.
  * If webhook mode is used, the bot is also wrapped in a dummy Express API so it can be run in an Azure App Service.
  */
-export default async function launchBotBasedOnNodeEnv<T extends Context>(
+export default function launchBotBasedOnNodeEnv<T extends Context>(
   bot: Bot<T, Api<RawApi>>,
 ) {
   const useWebhook = process.env.NODE_ENV === "production";
 
   if (useWebhook) {
     console.log("Launching bot in webhook mode");
-    await launchWebhookBot(bot);
+    launchWebhookBot(bot);
   } else {
     console.log("Launching bot in long polling mode");
     launchLongPollBot(bot);
